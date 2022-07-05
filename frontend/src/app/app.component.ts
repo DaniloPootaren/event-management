c5d86ee526b671bdaa33d2894ccf24a1df2d6dd5
Jean Danilo Maxwell Patrick Pootaren <danilo.pootaren@mns.mu>
2022-07-05 20:23:51 +0400

    menu WIP 10%

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ViewChild,
+  OnInit,
+  AfterViewInit,
+  HostListener,
+} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event: any) {
+    console.log('testing')
+    if (window.pageYOffset > 0) {
+      return (this.showAlternativeMenu = true);
+    }
+    return (this.showAlternativeMenu = false);
+  }
   title = 'frontend';
+  showAlternativeMenu = false;
+
+  ngOnInit(): void {}
 }
