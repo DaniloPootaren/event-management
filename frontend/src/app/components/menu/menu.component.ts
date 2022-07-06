cb7835ace612d492284c13ac0201726e77d9d95d
Jean Danilo Maxwell Patrick Pootaren <62770019+Daniloxax@users.noreply.github.com>
2022-07-06 13:19:05 +0400

    Added sideNav

--- frontend/src/app/components/menu/menu.component.ts
+++ frontend/src/app/components/menu/menu.component.ts
@@ -1,21 +1,32 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Inject, Input, OnInit } from '@angular/core';
 import { faAngleDown } from '@fortawesome/free-solid-svg-icons';
-import {  faCreditCard }from '@fortawesome/free-regular-svg-icons';
+import { faCreditCard } from '@fortawesome/free-regular-svg-icons';
 
 @Component({
   selector: 'menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.scss']
+  styleUrls: ['./menu.component.scss'],
 })
 export class MenuComponent implements OnInit {
-
   @Input() alternative: boolean = false;
+  @Input() main: any = null;
   chevronIcon = faAngleDown;
   shoppingCartIcon = faCreditCard;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
   }
 
+  openNav(sideNav: any, overlay:any) {
+    sideNav.style.width = '450px';
+    overlay.style.display = 'block';
+    this.main.style.backgroundColor = 'black';
+  }
+
+  closeNav(sideNav: any, overlay:any) {
+    sideNav.style.width = '0';
+    this.main.style.marginLeft = '0';
+    overlay.style.display = 'none';
+  }
 }
